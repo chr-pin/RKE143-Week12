85c1fe1e5040558c4e2fea0b635eafac76ff2011
Chr P <christian.pintson@tktk.ee>
2023-12-12 12:44:15 +0200

    parandus

--- routes/randomRecipe.routes.js
+++ routes/randomRecipe.routes.js
@@ -8,18 +8,18 @@ router.get('/', async (req, res) => {
 
         const receipeQuery = 'SELECT id, recipeName, instructions FROM receipe ORDER BY RANDOM() LIMIT 1;'
 
-    const receipeResult = await db.query(receipeQuery);
-    const selectedRecipe = receipeResult.rows[0];
+        const receipeResult = await db.query(receipeQuery);
+        const selectedRecipe = receipeResult.rows[0];
 
-    const ingredientsQuery = 'SELECT b.ingredientName FROM ingredient b INNER JOIN IngredientInRecipe c ON b.id = c.ingredientId WHERE c.recipeId = $1;'
+        const ingredientsQuery = 'SELECT b.ingredientName FROM ingredient b INNER JOIN IngredientInRecipe c ON b.id = c.ingredientId WHERE c.recipeId = $1;'
 
-    const ingredientRwsult =  await db.query(ingredientsQuery, [selectedRecipe.id]);
+        const ingredientRwsult =  await db.query(ingredientsQuery, [selectedRecipe.id]);
 
-    const ingredients = ingredientRwsult.rows.map(element => element.ingredientname)
+        const ingredients = ingredientRwsult.rows.map(element => element.ingredientname)
 
 
 
-    const randomRecipe = {
+        const randomRecipe = {
         recipe : selectedRecipe,
         ingredients: ingredients
     }
