53e039883d3f5beb2b7fd33ed2e07cce35aec83f
chr-pin <113059179+chr-pin@users.noreply.github.com>
2023-12-14 12:40:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes.js')
 const randomrouter = require('./routes/randomRecipe.routes.js')
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 app.use(express.json())
 
